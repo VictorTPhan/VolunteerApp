89f9cac8528108827d943d8b2db59a9640bd1969
Serene05 <109122353+Serene05@users.noreply.github.com>
2022-08-26 20:01:17 -0700

    volunteer info page format change

--- lib/volunteer_info.dart
+++ lib/volunteer_info.dart
@@ -174,17 +174,103 @@ class _VolunteerInfoState extends State<VolunteerInfo> {
       appBar: AppBar (
         title: Text ("Volunteer Info"),
       ),
-      body: Center(
-        child: Column (
-          children: [
-            Text(userName),
-            Text(description),
-            Text("Age: " + age),
-            Text("Instrument(s): " + instrument),
-            Text("Phone Number: " + phoneNumber),
-            Text("Email: " + email),
-          ],
-        ),
+      body: Column (
+        children: [
+          Expanded(
+              flex: 25,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      height: 150,
+                      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png")
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            userName,
+                            style: TextStyle(
+                                fontSize: 33,
+                                fontWeight: FontWeight.bold
+                            )
+                        ),
+                        Text(
+                            "volunteer",
+                             style: TextStyle(
+                               fontSize: 24
+                             ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+          ),
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(
+                    color: Colors.black,
+                    height: 27,
+                    thickness: 1.8,
+                  ),
+                  Text(
+                    "age: "+age,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    height: 27,
+                    thickness: 1.8,
+                  ),
+                  Text(
+                    "email: "+email,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                      height: 27,
+                    thickness: 1.8,
+                  ),
+                  Text(
+                      "phone number: "+phoneNumber,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                      height: 27,
+                      thickness: 1.8,
+                  ),
+                  Text("instrument: "+instrument,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    height: 27,
+                    thickness: 1.8,
+                  ),
+                  Text(description,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              flex: 75,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
